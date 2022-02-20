a893b8770d9e0951f466c48d00cf1e89e231ea10
Triskaaa <96921976+Triskaaa@users.noreply.github.com>
2022-02-20 10:21:26 +0100

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,14 @@
+module.exports =  {
+    name: "help",
+    code: `$title[1;Help Menu]
+    $description[1;:tada: **Giveaway :**
+        \`gstart/gcreate\`, \`gend\`, \`greroll\`
+    
+    🎁 **Utility :**
+    \`setprefix\`, \`ping\`
+
+👑 **Owner :**
+\`setavatar\`]
+    $addTimestamp[1]
+    $color[1;#ffd2cd]`
+}
